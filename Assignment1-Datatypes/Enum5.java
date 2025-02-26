58495cc4c64845fb8f94e970ffddc84ddcce8022
SaiPrasathS03 <148991466+SaiPrasathS03@users.noreply.github.com>
2025-02-26 14:13:00 +0530

    Update Enum5.java

--- Assignment1-Datatypes/Enum5.java
+++ Assignment1-Datatypes/Enum5.java
@@ -6,7 +6,8 @@ public class Enum5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int day = sc.nextInt();
-		System.out.println(Weekdays.values()[day-1]);
+		if(day>=0 && day<=7) System.out.println(Weekdays.values()[day-1]);
+		else System.out.println("Enter a valid input");
 	}
 	public enum Weekdays{
 		SUNDAY,MONDAY,TUESDAY,WEDNESDAY,THURSDAY,FRIDAY,SATURDAY;
